e884f2f5e6f60ac74575c748817ff9a841e78dc7
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-01-01 20:01:09 +0000

    Maqueto el proyecto

--- consumo-de-api/main.js
+++ consumo-de-api/main.js
@@ -4,7 +4,7 @@ const url = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_qCL
 const img1 = document.querySelector("#img1");
 const img2 = document.querySelector("#img2");
 const img3 = document.querySelector("#img3");
-const btn = document.querySelector("button");
+const btn = document.querySelector("#recargar");
 
 async function loadFetch (api) {
     const response = await fetch(api);
