0dbeb680121f80bc1baa88a00cf02d4fae7e0856
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-01-01 19:29:23 +0000

    Ejemplo de consumir una api

---
+++ consumo-de-api/main.js
@@ -0,0 +1,23 @@
+
+const url = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_qCLEUdso1SkWAyZQAzmzaQEVoXDe0Pk5RqDd3K1p5nPsYHs2muoPvMgboWxnFqSn";
+
+const img1 = document.querySelector("#img1");
+const img2 = document.querySelector("#img2");
+const img3 = document.querySelector("#img3");
+const btn = document.querySelector("button");
+
+async function loadFetch (api) {
+    const response = await fetch(api);
+    const data = await response.json();
+    return data;
+}
+
+btn.addEventListener("click", async () => {
+    const catImg = await loadFetch(url);
+    console.log(catImg);
+    img1.src = catImg[0].url;
+    img2.src = catImg[1].url;
+    img3.src = catImg[2].url;
+});
+
+
