be1800b19ee276ce197617b89a0124092afd183b
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-01-02 21:58:41 +0000

    Haciendo POST guardando en favoritos

--- consumo-de-api/main.js
+++ consumo-de-api/main.js
@@ -1,23 +1,94 @@
 
-const url = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_qCLEUdso1SkWAyZQAzmzaQEVoXDe0Pk5RqDd3K1p5nPsYHs2muoPvMgboWxnFqSn";
+const URL_RANDOM_CATS = "https://api.thecatapi.com/v1/images/search?limit=2";
+const URL_FAVORITES_CATS = "https://api.thecatapi.com/v1/favourites";
 
 const img1 = document.querySelector("#img1");
 const img2 = document.querySelector("#img2");
-const img3 = document.querySelector("#img3");
-const btn = document.querySelector("#recargar");
 
-async function loadFetch (api) {
-    const response = await fetch(api);
-    const data = await response.json();
+const btnRandomCats = document.querySelector("#recargar");
+const btnSaveCats = document.querySelector("#guardar1");
+
+const spanError = document.querySelector("#error");
+
+async function loadFetch (api, post = null) {
+
+    let response;
+
+    if (post === null) {
+        response = await fetch(api);
+    }
+    else {
+        response = await fetch(api, post);
+    }
+    
+    try {
+        if (response.status !== 200) {
+            spanError.innerHTML = `Hubo un error ${response.status}`;
+            console.log("entro");
+        }
+        data = await response.json();
+    }
+    catch(error) {
+        console.error(error);
+        console.log(data);
+    }
+
     return data;
 }
 
-btn.addEventListener("click", async () => {
-    const catImg = await loadFetch(url);
-    console.log(catImg);
+async function loadRandomCats() {
+    const catImg = await loadFetch(URL_RANDOM_CATS, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": "live_qCLEUdso1SkWAyZQAzmzaQEVoXDe0Pk5RqDd3K1p5nPsYHs2muoPvMgboWxnFqSn"
+        }
+    });
     img1.src = catImg[0].url;
     img2.src = catImg[1].url;
-    img3.src = catImg[2].url;
+    console.log(catImg);
+}
+
+async function loadFavoritesCats() {
+    const catImg = await loadFetch(URL_FAVORITES_CATS, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": "live_qCLEUdso1SkWAyZQAzmzaQEVoXDe0Pk5RqDd3K1p5nPsYHs2muoPvMgboWxnFqSn"
+        }
+    });
+
+    console.log(catImg);
+}
+
+async function saveFavoritesCats() {
+    const rest = await loadFetch(URL_FAVORITES_CATS, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": "live_qCLEUdso1SkWAyZQAzmzaQEVoXDe0Pk5RqDd3K1p5nPsYHs2muoPvMgboWxnFqSn"
+        },
+        body: JSON.stringify({
+            image_id: "4n0"
+        })
+    });
+
+    
+
+    console.log(rest.message);
+    console.log(rest.status);
+    console.log(rest);
+}
+
+btnRandomCats.addEventListener("click", async () => {
+    loadRandomCats();
 });
+
+btnSaveCats.addEventListener("click", async () => {
+    saveFavoritesCats();
+});
+
+loadFavoritesCats();
+
 
 
